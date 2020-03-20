fca934dd08ef1dcfa240aaf0616f337ba2b2cf63
unknown <ashkaell24@gmail.com>
2020-03-20 19:36:53 +0200

    add desktop style

--- src/components/Servises/Services.js
+++ src/components/Servises/Services.js
@@ -7,18 +7,20 @@ export const Services = () => {
     <div className="services">
       <h1 className="services__header">Услуги</h1>
       <ul className="services__tile">
-        {tileItems.map(item => {
+        {tileItems.map((item, index) => {
+          const { matches } = window.matchMedia('(min-width: 769px)')
+          const image = matches ? item.imageDesk : item.image
           return (
-            <li className="services__tile__item">
-              <div
-                className="services__tile__item_img"
-                style={{ backgroundImage: `url(${item.image})` }} />
+            <li
+              className="services__tile__item"
+              key={index}
+              style={{ backgroundImage: `url(${image})` }} >
               <p className="services__tile__item_title">{item.title}</p>
             </li>
           )
         })}
       </ul>
-      <input className="services__button" type="button" value="Расчет стоимости" />
+      <input className="services__button" type="button" value="Быстрый расчет цены по чертежу" />
     </div>
   )
 }
--- src/components/Servises/tileItems.js
+++ src/components/Servises/tileItems.js
@@ -1,42 +1,59 @@
-import Rectangle1 from '../../assets/images/tile-item/Rectangle1.png'
-import Rectangle2 from '../../assets/images/tile-item/Rectangle2.png'
-import Rectangle3 from '../../assets/images/tile-item/Rectangle3.png'
-import Rectangle4 from '../../assets/images/tile-item/Rectangle4.png'
-import Rectangle5 from '../../assets/images/tile-item/Rectangle5.png'
-import Rectangle6 from '../../assets/images/tile-item/Rectangle6.png'
-import Rectangle7 from '../../assets/images/tile-item/Rectangle7.png'
-import Rectangle8 from '../../assets/images/tile-item/Rectangle8.png'
+import Rectangle1 from '../../assets/images/mobile/tile-item/Rectangle1.png'
+import Rectangle2 from '../../assets/images/mobile/tile-item/Rectangle2.png'
+import Rectangle3 from '../../assets/images/mobile/tile-item/Rectangle3.png'
+import Rectangle4 from '../../assets/images/mobile/tile-item/Rectangle4.png'
+import Rectangle5 from '../../assets/images/mobile/tile-item/Rectangle5.png'
+import Rectangle6 from '../../assets/images/mobile/tile-item/Rectangle6.png'
+import Rectangle7 from '../../assets/images/mobile/tile-item/Rectangle7.png'
+import Rectangle8 from '../../assets/images/mobile/tile-item/Rectangle8.png'
+
+import DeskTopImage1 from '../../assets/images/desktop/tile-item/Rectangle1.png'
+import DeskTopImage2 from '../../assets/images/desktop/tile-item/Rectangle2.png'
+import DeskTopImage3 from '../../assets/images/desktop/tile-item/Rectangle3.png'
+import DeskTopImage4 from '../../assets/images/desktop/tile-item/Rectangle4.png'
+import DeskTopImage5 from '../../assets/images/desktop/tile-item/Rectangle5.png'
+import DeskTopImage6 from '../../assets/images/desktop/tile-item/Rectangle6.png'
+import DeskTopImage7 from '../../assets/images/desktop/tile-item/Rectangle7.png'
+import DeskTopImage8 from '../../assets/images/desktop/tile-item/Rectangle8.png'
 
 export const tileItems = [
   {
+    imageDesk: DeskTopImage1,
     image: Rectangle1,
     title: 'Производство оборудования'
   },
   {
+    imageDesk: DeskTopImage2,
     image: Rectangle2,
     title: 'Металлическая мебель'
   },
   {
+    imageDesk: DeskTopImage3,
     image: Rectangle3,
     title: 'Металлоконструкции'
   },
   {
+    imageDesk: DeskTopImage4,
     image: Rectangle4,
     title: 'Металлообработка'
   },
   {
+    imageDesk: DeskTopImage5,
     image: Rectangle5,
     title: 'Раскрой металла'
   },
   {
+    imageDesk: DeskTopImage6,
     image: Rectangle6,
     title: 'Конструкторское бюро'
   },
   {
+    imageDesk: DeskTopImage7,
     image: Rectangle7,
     title: 'Аренда техники'
   },
   {
+    imageDesk: DeskTopImage8,
     image: Rectangle8,
     title: 'Ремонт техники'
   },
